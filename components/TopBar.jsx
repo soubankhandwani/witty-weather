a521e21a874d994fe3d9c02a443dbec35d794633
Souban Khandwani <90709155+Beastnottooop@users.noreply.github.com>
2023-06-03 11:44:58 +0530

    Update TopBar.jsx

--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -13,7 +13,7 @@ const TopBar = () => {
             </div>
             <div className="flex items-center text-white">
                 <div className="ml-4">
-                    <h1 className="text-2xl md:text-3xl lg:text-3xl font-bold pb-1">Name</h1>
+                    <h1 className="text-2xl md:text-3xl lg:text-3xl font-bold pb-1">WittyWeather</h1>
                     <h5 className="text-sm md-text-xl lg:text-xl font-thin">Weather updates with a side of wit!</h5>
                 </div>
             </div>
